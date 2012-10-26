f0d82ad4f1c3e32c96b9fd75b39e4c912cb1586f
Steve Laing <steve.laing@gmail.com>
2012-10-26 21:37:28 +0100

    Added colorbox gallery injection

--- app.rb
+++ app.rb
@@ -8,14 +8,25 @@ before do
     :secret_access_key => ENV['S3_SECRET'])
 end
 
+helpers do
+  def image_objects(bucket, folder)
+    s3objects = AWS::S3::Bucket.objects(params[:bucket], prefix: params[:folder])
+  
+    s3objects.map(&:url).select! { |url|
+      url =~ /\w+\.(gif|jpg|jpeg|png)/ # TODO: Handle other media
+    }
+  end
+end
+
 get '/' do
   "GET /:bucket/:folder"  
 end
 
 get '/:bucket/:folder' do
-  s3objects = AWS::S3::Bucket.objects(params[:bucket], prefix: params[:folder])
-  
-  json s3objects.map(&:url).select! { |url|
-    url =~ /\w+\.(gif|jpg|jpeg|png)/ # TODO: Handle other media
-  }
+  json image_objects(params[:bucket], params[:folder]) 
+end
+
+get '/galleryjs/:bucket/:folder/:element_id' do
+  @urls = json image_objects(params[:bucket], params[:folder])
+  erb "s3gallery.js".to_sym
 end
