b8ce0cbc2abad67a8f78036c602aad3aeb13d286
Steve Laing <steve.laing@gmail.com>
2012-11-05 21:34:37 +0000

    Add S3 test helper methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,37 @@
 ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
 require 'rack/test'
+require 'json'
 
 begin
   require_relative '../app'
 rescue NameError 
   require File.expand_path('../app', __FILE__)
+end
+
+include Rack::Test::Methods
+def app() Sinatra::Application end
+
+module AWS
+  module S3
+    class Base
+      def self.establish_connection!(opts)
+        true
+      end
+    end
+    class Bucket
+      def self.objects(name, opts)
+        ['/picture/of/a/cat.jpg','/some/random/folder',
+         '/picture/of/a/dog.png','/picture/of/fairies.bmp'].map do |url|
+            AWS::MockS3Object.new(url)
+        end
+      end
+    end
+  end
+  class MockS3Object
+    attr_reader :url
+    def initialize(url)
+      @url = url
+    end
+  end
 end
